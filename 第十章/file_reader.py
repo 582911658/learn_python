e17fd9e354e749e15b4e4aa1d2cf305f6e973079
xiaokang <582911658@qq.com>
2021-06-09 10:26:44 +0800

    提交

--- 第十章/file_reader.py
+++ 第十章/file_reader.py
@@ -3,4 +3,4 @@ filename = 'pi_digits.txt'
 with open(filename) as file_project:
     lines = file_project.readlines()
     for line in lines:
-        print(line, end='')
+        print(line.rstrip())
--- 第十章/pi_string.py
+++ 第十章/pi_string.py
@@ -4,7 +4,10 @@ with open(file_name) as file_object:
     lines = file_object.readlines()
 pi_string = ''
 for line in lines:
-    pi_string += line.rstrip()
+    pi_string += line.strip()
+print(pi_string)
+print(len(pi_string))
+
 
 birthday = input('请输入你的生日，格式为mmddyy，例如900910\n')
 if birthday in pi_string:
